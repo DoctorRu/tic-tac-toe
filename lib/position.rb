5c0e7b8fb5a34d2a2c57c725a9d4cbd8af9b4b07
doctorru <vjblan@gmail.com>
2017-05-14 18:39:22 -0300

    gosu

--- lib/position.rb
+++ lib/position.rb
@@ -1,10 +1,9 @@
 class Position
-
     attr_accessor :board, :turn
     DIM = 3
     SIZE = DIM * DIM
 
-    def initialize(board = nil, turn="x")
+    def initialize(board=nil, turn="x")
         @board = board || %w(-) * SIZE
         @turn = turn
     end
@@ -20,15 +19,31 @@ class Position
     end
 
     def possible_moves
-        @board.map.with_index { |p, i| i if p == "-" }.compact
+        @board.map.with_index { |p, i| i if p=="-" }.compact
     end
 
     def win?(turn)
         rows = @board.each_slice(DIM).to_a
-        rows.any? { |row| row.all? { |p| p == turn } } ||
-            rows.transpose.any? { |col| col.all? { |p| p == turn } } ||
-            rows.map.with_index.all? { |row, i| row[i] == turn } ||
-            rows.map.with_index.all? { |row, i| row[DIM - 1 - i] == turn }
+        rows.any? { |row| row.all? { |p| p==turn } } ||
+            rows.transpose.any? { |col| col.all? { |p| p==turn } } ||
+            rows.map.with_index.all? { |row, i| row[i]==turn } ||
+            rows.map.with_index.all? { |row, i| row[DIM-i-1]==turn }
 
+    end
+
+    def minimax(depth=1)
+        return 100 if win?("x")
+        return -100 if win?("o")
+        return 0 if possible_moves.empty?
+        @@minimax ||= {}
+        value = @@minimax[@board]
+        return value if value
+        @@minimax[@board] = possible_moves.map { |idx|
+            move(idx).minimax(depth+1)
+        }.send(xturn(:max, :min)) + xturn(-depth, depth)
+    end
+
+    def best_move
+        possible_moves.send(xturn(:max_by, :min_by)) { |idx| move(idx).minimax }
     end
 end
